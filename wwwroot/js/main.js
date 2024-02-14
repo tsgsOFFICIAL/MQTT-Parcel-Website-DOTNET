96f66cd27a31a20f89d4e22f126e61c7325142f9
Marcus Jensen <marcusproman0507@gmail.com>
2024-02-14 09:02:37 +0100

    Notification

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -1,28 +1,15 @@
-let registration;
-
-topLevelFunction();
-
-async function topLevelFunction() {
-    registration = await navigator.serviceWorker.getRegistration();
-}
-
-// Function to send a notification
-async function sendNotification(notifyTitle, notifyBody) {
-    if (Notification.permission === 'granted') {
-        showNotification(notifyTitle, notifyBody);
-    } else {
-        if (Notification.permission !== 'denied') {
-            const permission = await Notification.requestPermission();
-
-            if (permission === 'granted') {
-                showNotification(notifyTitle, notifyBody);
-            }
-        }
+function requestPermission() {
+    if (!('Notification' in window)) {
+        alert('Notification API not supported!');
+        return;
     }
+
+    Notification.requestPermission((result) => {
+        console.log(result);
+    });
 }
 
-// Function to show a notification
-function showNotification(notifyTitle, notifyBody) {
+function showNonPersistentNotification(notifyTitle, notifyBody) {
     const notifyImg = `./assets/icons/icon-128x128.png`;
 
     const payload = {
@@ -30,62 +17,41 @@ function showNotification(notifyTitle, notifyBody) {
         icon: notifyImg,
     };
 
-    if ('showNotification' in registration) {
-        registration.showNotification(notifyTitle, payload);
-    } else {
-        new Notification(notifyTitle, payload);
+    if (!('Notification' in window)) {
+        alert('Notification API not supported!');
+        return;
+    }
+
+    try {
+        const notification = new Notification(notifyTitle, payload);
+
+        // Event listener when the user clicks on the notification
+        notification.onclick = () => {
+            console.log('The notification was clicked');
+        };
+    } catch (err) {
+        alert('Notification API error: ' + err);
     }
 }
 
+function showPersistentNotification(notifyTitle, notifyBody) {
+    const notifyImg = `./assets/icons/icon-128x128.png`;
 
+    const payload = {
+        body: notifyBody,
+        icon: notifyImg,
+    };
 
+    if (!('Notification' in window) || !('ServiceWorkerRegistration' in window)) {
+        alert('Persistent Notification API not supported!');
+        return;
+    }
 
-
-
-
-//let registration;
-
-
-//const sendNotification = async (notifyTitle, notifyBody) => {
-//    const permission = await Notification.requestPermission();
-
-//    if (permission === 'granted') {
-//        const notifyImg = `./assets/icons/icon-128x128.png`;
-
-//        const payload = {
-//            body: notifyBody,
-//            icon: notifyImg,
-//        };
-
-//        if ('showNotification' in registration) {
-//            registration.showNotification(notifyTitle, payload);
-//        } else {
-//            const notification = new Notification(notifyTitle, payload);
-
-//            // Event listener when the user clicks on the notification
-//            notification.onclick = () => {
-//                console.log('The notification was clicked');
-//            };
-//        }
-//    }
-//};
-
-
-//const requestPermission = async () => {
-//    if (Notification.permission !== 'granted') {
-//        await Notification.requestPermission();
-//    }
-//};
-
-//// Function to get service worker registration and request permission
-//const initialize = async () => {
-//    try {
-//        registration = await navigator.serviceWorker.getRegistration();
-//        await requestPermission();
-//    } catch (error) {
-//        console.error('Error initializing service worker:', error);
-//    }
-//};
-
-//// Initialize the service worker and request permission
-//initialize();
+    try {
+        navigator.serviceWorker.getRegistration()
+            .then((reg) => reg.showNotification(notifyTitle, payload))
+            .catch((err) => alert('Service Worker registration error: ' + err));
+    } catch (err) {
+        alert('Notification API error: ' + err);
+    }
+}
